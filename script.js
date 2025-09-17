532083c87df729a6bdb0954ea2b0ab66d2992543
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-09-17 13:54:24 +0200

    modal-window-style

--- script.js
+++ script.js
@@ -9,15 +9,29 @@ btnContact.addEventListener('click', () => {
 // Error modal window
 const btnViewProjects = document.getElementById('btn-view-projects');
 btnViewProjects.addEventListener('click', () => {
-    createWindow();
+    createWindow("All good. But we do not have a content to show.", "alert-good");
 });
 
 const btnSend = document.getElementById('send');
 btnSend.addEventListener('click', (e) => {
     // Prevents the form from submitting
     e.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let text = document.getElementById('message').value;
     
-    createWindow();
+    name = name.trim();
+    email = email.trim();
+    text = text.trim();
+
+    if(!name || !email || !text){
+        createWindow("Warnings. Check input values.", "alert-warn");
+    }
+    else{
+        createWindow("Error. Try again or change your message.", "alert-error");
+    }
+
 });
 
 
@@ -31,8 +45,8 @@ modal.style.zIndex = "1000";
 document.body.appendChild(modal);
 
 // function to create modal window
-const createWindow = () => {
-    modal.innerHTML = renderWindow();
+const createWindow = (message, type) => {
+    modal.innerHTML = renderWindow(message, type);
     modal.style.display = "flex";
 
     document.getElementById("close-modal").onclick = () => {
@@ -44,13 +58,13 @@ const createWindow = () => {
     };
 };
 // helper to render
-const renderWindow = () => {
+const renderWindow = (message, type = "alert-good") => {
     return `
         <div class="modal-content">
-            <div>
-                <p>Error!<p>
+            <div class="${type}">
+                <p>${message}<p>
             </div>
-            <div style="display: flex; justify-content: space-between">
+            <div style="display: flex; justify-content: center; margin-top: 16px;">
                 <button id="close-modal">Close</button>
             </div>
         </div>
